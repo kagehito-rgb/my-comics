72db856c807463ebdbaa442e95845011108e28f1
kagehito-rgb <npu3tdz3mt@i.softbank.jp>
2021-02-16 02:36:20 +0900

    project作成時の初期ファイル類追加・gitignore追加

---
+++ my-comics/my_comicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_comicsApp.swift
+//  my-comics
+//
+//  Created by kagehito on 2021/02/16.
+//
+
+import SwiftUI
+
+@main
+struct my_comicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
